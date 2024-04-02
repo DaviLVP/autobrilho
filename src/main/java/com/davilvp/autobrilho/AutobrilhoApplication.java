f89962a1b4998c80cb14006586cc9fa0a638d9a8
DaviLVP <davipedrosa1981@gmail.com>
2024-04-02 20:24:11 -0300

    commit inicial

---
+++ src/main/java/com/davilvp/autobrilho/AutobrilhoApplication.java
@@ -0,0 +1,13 @@
+package com.davilvp.autobrilho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobrilhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobrilhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davilvp/autobrilho/AutobrilhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davilvp.autobrilho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobrilhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
